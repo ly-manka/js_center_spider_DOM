fa777c4981951a83da317086c73155e3ce7e9876
iryna lysko <goiralysko@gmail.com>
2024-08-13 16:56:52 +0300

    make build

---
+++ index.96412478.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.clientWidth,c=e.clientHeight,l=document.querySelector(".spider"),i=l.clientWidth,n=l.clientHeight;l.style.top="".concat(c/2-n/2,"px"),l.style.left="".concat(t/2-i/2,"px");
+//# sourceMappingURL=index.96412478.js.map
